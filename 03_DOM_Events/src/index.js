661c4db0f76b6a1d411f1195e600f5b0790e0b24
Thomas Wolf <thomasrussellwolf@gmail.com>
2023-07-13 11:45:08 -0500

    lecture complete 7 13 2023

--- 03_DOM_Events/src/index.js
+++ 03_DOM_Events/src/index.js
@@ -1,4 +1,4 @@
-const bookList = document.querySelector('#book-list');
+const bookList = document.getElementById('book-list');
 function formatPrice(price) {
   return '$' + Number.parseFloat(price).toFixed(2);
 }
@@ -9,14 +9,14 @@ function formatPrice(price) {
 
 // create a function renderHeader() that takes the store name from bookStore and adds to the DOM
 function renderHeader(bookStore) {
-  document.querySelector('#store-name').textContent = bookStore.name;
+  document.getElementById('store-name').textContent = bookStore.name;
 }
 
 function renderFooter(bookStore) {
-  document.querySelector('#location').textContent = bookStore.location;
-  document.querySelector('#number').textContent = bookStore.number;
-  document.querySelector('#address').textContent = bookStore.address;
-  document.querySelector('#hours').textContent = bookStore.hours;
+  document.getElementById('location').textContent = bookStore.location;
+  document.getElementById('number').textContent = bookStore.number;
+  document.getElementById('address').textContent = bookStore.address;
+  document.getElementById('hours').textContent = bookStore.hours;
 }
 
 // function: renderBook(book)
@@ -55,6 +55,9 @@ function renderBook(book) {
 
   const btn = document.createElement('button');
   btn.textContent = 'Delete';
+  btn.addEventListener("click", () => {
+    li.remove()
+  })
   li.append(btn);
 
   bookList.append(li);
@@ -67,5 +70,102 @@ function renderBook(book) {
 
 renderHeader(bookStore);
 renderFooter(bookStore);
+
 bookStore.inventory.forEach(renderBook);
 
+// for(book of bookStore.inventory){
+//   renderBook(book)
+//}
+
+
+////////////////////////////////////////////
+// Form Handling
+////////////////////////////////////////////
+const bookForm = document.getElementById("book-form")
+
+bookForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  //Get all data from the form
+  const title = bookForm.title.value
+  const author = bookForm.author.value
+  const price = bookForm.price.value
+  const imageUrl = bookForm.imageUrl.value
+  const inventory = bookForm.inventory.value
+
+  //put data into a book object
+  const newBook = {
+    title: title,
+    author:author,
+    price: price,
+    inventory:inventory,
+    imageUrl: imageUrl
+  }
+
+  renderBook(newBook)
+
+});
+
+
+hideButton = document.getElementById("toggleForm")
+
+toggleBook = (e) => {
+  //Hide the form to add a new book
+
+  //If the book form is currently not displayed
+  if (bookForm.style.display === "none") {
+    //show the book form.
+    bookForm.style.display = "block"
+    hideButton.textContent = "Hide Book Form"
+  } else {
+    //hide the book form.
+    bookForm.style.display = "none"
+    hideButton.textContent = "Show Book Form"
+  }
+}
+
+hideButton.addEventListener("click", toggleBook)
+
+
+// Event Listener:
+// hide the Book From by pressing the Escape key
+//  while it is visible.
+document.addEventListener('keydown', (event) => {
+  if (event.key === "Escape") {
+    if (bookForm.style.display !== "none") {
+      bookForm.style.display = "none";
+      hideButton.textContent = "Show Book Form"
+    }
+  }
+  // 
+})
+
+document.addEventListener()
+
+/* a comment */
+
+/*
+1. Users should be able to delete books 
+    by clicking the delete button 
+    at the bottom of the book card
+    [X]
+
+2. Users should be able to add a book by
+  filling in the form at the top of the page
+  (the book they add should appear in the DOM)
+  [X]
+
+3. Users should be able to click the
+  Hide Book Form button to hide the 
+  form to add a new book
+  [X]
+
+4. Users should be able to show 
+  the New Book form by clicking
+  the button again
+  [X]
+Bonus: Users should be able to hide the
+  Book From by pressing the Escape key
+  while it is visible.
+  [X]
+  */
